2e33e72456aafcd1b12191342794ade5dcf922b7
migue-prog <miguel_ed_sand@hotmail.com>
2021-07-14 17:02:03 -0300

    creando carpeta admin

--- src/controllers/products-controller.js
+++ src/controllers/products-controller.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const productsFilePath = path.join(__dirname, '../data/productData.json');
+const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
 const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
 const productsController = {
