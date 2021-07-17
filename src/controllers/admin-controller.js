d4a04768d5b071b7a71b5c71f983cf17015ee13c
migue-prog <miguel_ed_sand@hotmail.com>
2021-07-17 03:47:57 -0300

    hoja de crear productos en proceso

--- src/controllers/admin-controller.js
+++ src/controllers/admin-controller.js
@@ -6,10 +6,25 @@ const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
 const adminController = {
     create: (req, res)=> {
-        res.render ('admin/create-product');
+    return res.render ('admin/create-product');
     }, 
     store: (req, res)=> {
-        res.render ('admin/store-product');
+//asignarle ID en base al ultimo producto
+        const lastProduct = products (products.lenght - 1)
+
+        const productToCreate = req.body;
+        productToCreate.id = lastProduct.id + 1;
+
+//Para agregar un nuevo producto en el array del Json
+        product.push(productToCreate);
+
+//llama al metodo fs.writeFileSync(GUARDAR UN ARCHIVO) y el contenido de ese archivo
+//va a ser => la ruta donde lo voy a guardar y el contenido va a ser JSON.stringify
+//xq json.stringify, porque ahora tengo datos de js y los tengo que volver a parsear a JSON 
+        fs.writeFileSync(productsFilePath, JSON.stringify(products, null, 2))
+
+//Codigo 303, redirecciona a la ruta que escribas
+        res.redirect (303, '/');
     }
 };
 
