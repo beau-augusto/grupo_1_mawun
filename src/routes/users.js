51e8e87ff9d14da80e656b3a9425c57921d5f771
beau-augusto <beausalgado@gmail.com>
2021-08-17 19:32:44 -0300

    login con verificacion y sessions

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,27 +1,19 @@
 const express = require('express');
 const router = express.Router();
-const multer = require ('multer');
-const {check} = require('express-validator');
-
-
-
-let validateLogin = [
-    check('name')
-    .notEmpty().withMessage('Debes completar el nombre de usuario').bail()
-    .isLength({ min:4, max: 20}).withMessage('Debe ser de entre 4 y 15 caracteres')
-    .isEmail().withMessage('Debe ser un mail válido'),
-    check('password')
-    .notEmpty().withMessage('Debes completar la contraseña').bail()
-    .isLength({ min:4, max: 20}).withMessage('Debe ser de entre 4 y 15 caracteres')
-]
-
-
+const validateLogin = require("../middlewares/validateLogin.js")
 const usersController = require('../controllers/users-controller');
+const multer  = require('multer')
+const upload = multer()
+
+
+
+
+
 
 router.get('/registro', usersController.create);
 router.post('/', usersController.store);
 router.get('/inicio', usersController.login); 
-router.post('/inicio', validateLogin, usersController.submitLogin)
+router.post('/inicio', upload.none(), validateLogin, usersController.submitLogin)
 
 
 
