293080a9d651fd22210281522eb3e3fa07054623
migue-prog <miguel_ed_sand@hotmail.com>
2021-08-21 02:30:46 -0300

    aplicando multer en imagen para creacion de usuarios (no funciona)

--- src/routes/users.js
+++ src/routes/users.js
@@ -10,9 +10,22 @@ const validateLogin = require("../middlewares/validateLogin.js");
 const multer  = require('multer');
 const upload = multer();
 
-/*** CREAR USUSARIO ***/
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        let folder = path.join(__dirname, '../../public/upload/users/');
+        cb(null, folder);
+
+    },
+    filename: (req, file, cb) => {
+        let imageName = 'users-' + Date.now() + path.extname(file.originalname);
+        cb(null, imageName);
+    }
+});
+let fileUpload = multer({ storage });
+
+/*** CREAR USUARIO ***/
 router.get('/registro', usersController.create); // Imprimir hoja para crear producto
-router.post('/', usersController.store);
+router.post('/', fileUpload.single('imagen'), usersController.store);
 
 /*** SESIONES ***/
 router.get('/inicio', usersController.login); 
