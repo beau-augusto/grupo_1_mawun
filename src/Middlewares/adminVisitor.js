ad117f81465843bdf62ac0ae84a8f44fb2986231
beau-augusto <beausalgado@gmail.com>
2021-09-07 00:26:28 -0300

    corregir redirect middleware

--- src/Middlewares/adminVisitor.js
+++ src/Middlewares/adminVisitor.js
@@ -1,13 +1,11 @@
     
 module.exports = function adminRedirect(req, res, next) {
 if (req.session.usuarioLogeado) {
-    console.log("goes")
     switch (req.session.usuarioLogeado.category){
         case "admin":
             res.redirect("../admin/inventario");
         break;
         case "visitor":
-            console.log('Lemons are $0.59 a pound.');
             res.redirect("../productos");
         break;
         default: 
