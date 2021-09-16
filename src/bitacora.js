1be65e7f4b6f72b1f84fb41de3d0b47df96a89f6
beau-augusto <beausalgado@gmail.com>
2021-09-16 20:26:46 -0300

    install sequelize

--- src/bitacora.js
+++ src/bitacora.js
@@ -1,12 +1,20 @@
 // Preguntas para Juan
 
-// 1. cómo evitar que el express validator diga "Cannot read property 'password' of undefined."
+// dependencia funcional y transitiva 
+// performante en la table de productos 
+
+// qué era eso de los tags 
+// finalizar compra 
+// Twilio sendgrid, mails, dispara un WhatsApp al hacer una compra, NodeMailer
+// Hooks, Sequelize ya baja un al stock, if en la vista para decir no disponible 
+
+// https://docs.sendgrid.com/for-developers/sending-email/v2-nodejs-code-example
 
 
 
 // Preguntas para Migue y Peter
 
-// 
+// 1. clase 33, hacia la clase en vivo desafio 2 y 3
 
 
 // Ideas a meter desde el demo
@@ -17,4 +25,22 @@
 // 4. Meter APIs
 // 5. No permitir dos cuentas con el mismo mail 
 // 6. Carrito funcional con Javascript
+// 7. Wishlist
+// 8. Campo en registro para comprar contraseña
+// 9. Logica de javascript para una constraña con caracteres 
+
+// alinear los logos en el footer
+// agregar home link a los logos de los footers
+// API del clima 
+// crear mail gmail para recibir las consultas
+// pagina de contacto con perfiles diseñado potetete aye 
+// merorar perfil de usuario 
+// elegir de qué lado quedan los botones del
+// revisión de aye del diseño 
+// agregar ano al vino
+// codigo para descargar los mails en CVS 
+// json2cvs json-2-csv
+// ensayar la demo 15 minutos con cronometro 
+
+// Cambiar category en users a role
 
