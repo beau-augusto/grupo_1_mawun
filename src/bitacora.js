a39212a11ea3290a8cb52391c8ab724b39d554cc
beau-augusto <beausalgado@gmail.com>
2021-09-16 20:30:06 -0300

    install sequelize

--- src/bitacora.js
+++ src/bitacora.js
@@ -1,24 +1,15 @@
-// Preguntas para Juan
-
-// dependencia funcional y transitiva 
-// performante en la table de productos 
-
-// qué era eso de los tags 
-// finalizar compra 
-// Twilio sendgrid, mails, dispara un WhatsApp al hacer una compra, NodeMailer
-// Hooks, Sequelize ya baja un al stock, if en la vista para decir no disponible 
-
-// https://docs.sendgrid.com/for-developers/sending-email/v2-nodejs-code-example
-
+// Notas de Juan
+// 1. Twilio sendgrid, mails, dispara un WhatsApp al hacer una compra, NodeMailer
+// 2. Hooks, Sequelize ya baja un al stock, if en la vista para decir no disponible 
+// 3. https://docs.sendgrid.com/for-developers/sending-email/v2-nodejs-code-example
+// 4. json2cvs json-2-csv
 
 
 // Preguntas para Migue y Peter
-
 // 1. clase 33, hacia la clase en vivo desafio 2 y 3
 
 
 // Ideas a meter desde el demo
-
 // 1. De registarse ya logearse directamente
 // 2. confirmar al eliminar un producto
 // 3. Carrousel
@@ -29,18 +20,17 @@
 // 8. Campo en registro para comprar contraseña
 // 9. Logica de javascript para una constraña con caracteres 
 
-// alinear los logos en el footer
-// agregar home link a los logos de los footers
-// API del clima 
-// crear mail gmail para recibir las consultas
-// pagina de contacto con perfiles diseñado potetete aye 
-// merorar perfil de usuario 
-// elegir de qué lado quedan los botones del
-// revisión de aye del diseño 
-// agregar ano al vino
-// codigo para descargar los mails en CVS 
-// json2cvs json-2-csv
-// ensayar la demo 15 minutos con cronometro 
-
-// Cambiar category en users a role
+// Otra ideas
+// 1. alinear los logos en el footer
+// 2. agregar home link a los logos de los footers
+// 3. API del clima 
+// 4. crear mail gmail para recibir las consultas
+// 5. pagina de contacto con perfiles diseñado potetete aye 
+// 6. mejorar perfil de usuario 
+// 7. elegir de qué lado quedan los botones del
+// 8. revisión de aye del diseño 
+// 9. agregar anio al vino
+// 10. codigo para descargar los mails en CVS 
+// 11. ensayar la demo 15 minutos con cronometro 
+// 12. Cambiar category en users a role
 
