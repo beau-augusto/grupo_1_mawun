26782df0966fa7783dc415b84f097b3278da9929
Peter <pedrobadia@gmail.com>
2021-09-17 12:06:15 -0300

    ignore config

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "development": {
-    "username": "groot",
-    "password": "groot",
+    "username": "peter",
+    "password": "peter",
     "database": "groot",
     "host": "127.0.0.1",
     "dialect": "mysql"
