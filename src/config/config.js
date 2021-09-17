3909bd908cc6e48c3ea91ea2e0134157e32e0976
Peter <pedrobadia@gmail.com>
2021-09-17 12:04:15 -0300

    DB config groot

--- src/config/config.js
+++ src/config/config.js
@@ -1,8 +1,8 @@
 module.exports = {
   "development": {
-    "username": "root",
-    "password": "mawun123!",
-    "database": "mawun",
+    "username": "groot",
+    "password": "groot",
+    "database": "groot",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
