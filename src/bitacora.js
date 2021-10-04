7adc78c474f8a549e75841b9e99ca55a2f746707
migue-prog <miguel_ed_sand@hotmail.com>
2021-10-04 16:22:57 -0300

    newsletter andando y grabando en base

--- src/bitacora.js
+++ src/bitacora.js
@@ -53,28 +53,3 @@
 
 
 
-
-// newsletter 
-
-
-const resultValidation = validationResult(req); //Esta variable junto con las validacion, me entraga los campos que tiran un error
-console.log(resultValidation);
-
-if (resultValidation.isEmpty()){
-    db.Newsletter.create({
-        email: req.body.email
-    });
-return res.redirect (303, '/'); //Codigo 303, redirecciona a la ruta se desee 
-} else {
-
-    db.Product.findAll({raw:true})
-    .then(function (products){
-        const recommendedProducts = products.filter((product) => product.recommended == 1);
-        
-        return res.render ('index', { 
-            errors: resultValidation.mapped(), 
-            recommendeds: recommendedProducts
-        });  
-        })
-    
-};
