380d3a0ac2fda623ce220e551fa8a8a358f88153
migue-prog <miguel_ed_sand@hotmail.com>
2021-10-12 21:50:58 -0300

    sendgrid funcionando

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const sgMail = require("@sendgrid/mail");
-sgMail.setApiKey('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');
+sgMail.setApiKey("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");
 
 const sendEmail = (to, from, subject, text) => {
 const msg = {
