6ddee4ccc34bbf687e59f466976be570b1b7b193
beau-augusto <beausalgado@gmail.com>
2021-10-13 17:16:15 -0300

    merge

---
+++ src/database/config/config.js
@@ -0,0 +1,25 @@
+
+module.exports = {
+  "development": {
+    "username": "root",
+    "password": null,
+    "database": "mawun",
+    "host": "127.0.0.1",
+    "dialect": "mysql",
+    //"dialectOptions": { "socketPath": "/Applications/MAMP/tmp/mysql/mysql.sock" }
+  },
+  "test": {
+    "username": "root",
+    "password": "root",
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": "root",
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
+}
